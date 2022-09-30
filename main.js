8faaddb83433ae5d57a51b29d204f86a015f71f4
Mahmoud Eid <mahmoudeid1840@gmail.com>
2022-09-30 18:01:44 +0200

    position fixed and change landing pic

--- main.js
+++ main.js
@@ -11,3 +11,49 @@ backdrop.onclick=()=>{
     ul.style.display="none";
     backdrop.style.display="none";
  }
+
+ /*start to make scroll effect*/
+ let header=document.querySelector('.header');
+ let landing=document.querySelector('.landing');
+
+ window.onscroll=function(){
+   header.classList.add('scroll');
+   landing.style.height="100vh";
+ }
+ // Dark mode
+//select button
+let toggle=document.getElementById('toggleDark');
+//select body
+let body=document.querySelector('body');
+toggle.addEventListener('click',toggleDark);
+function toggleDark(){
+  this.style.transition="all 1s linear";
+  this.style.transform="rotate(360deg)";
+   this.classList.toggle('bi-moon');
+    if(toggle.classList.contains('bi-moon')){                      
+              body.classList.add('darkMode');
+    } else{
+            body.classList.remove('darkMode');
+            body.style.transition="2s";
+      
+      }
+  }
+  
+  //scroll to top
+  let up =document.querySelector('.up');
+  if(window.scrollY >= 45){
+   up.style.display="block";
+ } else{
+    up.style.display="none";
+ }
+
+ up.onclick=function(){
+   console.log(up)
+ }
+ up.addEventListener('click',()=>{
+   window.scrollTo({
+     left:0,
+     top:0,
+     behavior:"smooth",
+   })
+ })  
